cc790570d51cb4b5a1125b0f5063b913f981c8a8
matt roche <matt.roche@gmail.com>
2023-05-12 00:37:15 -0500

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [int**2 for int in int_list]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    count = 1
+    while 1 <= count <= 100:
+        if count % 3 == 0 and count % 5 == 0:
+            print("FizzBuzz")
+        elif count % 3 == 0:
+            print("Fizz")
+        elif count % 5 == 0:
+            print("Buzz")
+        else:
+            print(count)
+        count += 1
